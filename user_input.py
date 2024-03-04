226e56d960aab5d9fcf3ac5157a7f0c7867ed8f2
0ctoberZA <tsakanilindsay@gmail.com>
2024-03-04 12:50:48 +0200

    user input

---
+++ user_input.py
@@ -0,0 +1,7 @@
+# Assignment 1 User Input.
+
+name = input("what's your Name?")
+age = input("How old are you?")
+location = input("What is your location?")
+
+print("Hello{}, you are{} years old and live in {}.".format(name, age, location))
